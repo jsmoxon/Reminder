4f1e353617d649b8fda6d2c818d55a9e8d0ef5f3
Jack Moxon <jsmoxon@gmail.com>
2012-01-16 22:16:47 -0800

    asdkjf

--- reminder/mailstuff/views.py
+++ reminder/mailstuff/views.py
@@ -3,17 +3,15 @@ from reminder.constants import EMAIL_HOST_USER
 reply_email = '14af7a6e3133f0079033@cloudmailin.net'
 
 def create_post(**message):
-    author = User.objects.get(email=message['from'])
-    print author
     title = message['subject']
     content = message['plain']
-
-    p = Post.objects.create(
+    p = MailPost.objects.create(
         author = author,
         title = title, 
         content = content,
     )
-    print p
+    p.save()
+    
     send_mail (
         subject='New post created',
         message = content,
@@ -21,5 +19,5 @@ def create_post(**message):
         recipient_list=['jsmoxon@gmail.com'],
         fail_silently=True
     )
-    print p, author, subject
+
                               
